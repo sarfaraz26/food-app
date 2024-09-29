8ba7efee592c546f922fa2058953786be0f8bb64
Zoya Ansari <ansari.zoyaa13@gmail.com>
2024-09-29 21:31:19 +0530

    Added hardcoded cart screen and routing implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductsScreenComponent } from './screens/products-screen/products-screen.component';
+import { CartScreenComponent } from './screens/cart-screen/cart-screen.component';
+
+
+const routes : Routes = [
+  {
+    path : '', 
+    component : ProductsScreenComponent
+  },
+  {
+    path : 'cart',
+    component : CartScreenComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ProductsScreenComponent
+    ProductsScreenComponent,
+    CartScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
